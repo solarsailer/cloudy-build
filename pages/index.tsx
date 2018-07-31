ffd0cbe1b70d422e5b96c9eb7b3af2fde473f552
Matthieu Oger <moger@pixelnest.io>
2018-07-31 19:11:13 +0200

    Add inert fields to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
-import styled from 'styled-components'
+import Field from '../components/Field'
+import Button from '../components/Button'
 
 // -------------------------------------------------------------
 // Components.
 // -------------------------------------------------------------
 
-export default () => <p>Hello Unity Cloud Build!</p>
+export default () => (
+  <div id="get-latest-builds">
+    <form>
+      <Field id="org" description="Your company name, probably.">
+        Organization
+      </Field>
+
+      <Field
+        id="key"
+        placeholder="axp818372dqsd917dqa1708bqs4258ab"
+        description={
+          <>
+            Available in the{' '}
+            <a href="https://developer.cloud.unity3d.com/preferences/">
+              Unity Cloud Build preferences
+            </a>.
+          </>
+        }
+      >
+        API Key
+      </Field>
+    </form>
+
+    <Button type="submit">Get Latest Builds</Button>
+  </div>
+)
