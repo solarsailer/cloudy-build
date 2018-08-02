cf8fbfa89c1354db09c8589543b632955fb984e3
Matthieu Oger <moger@pixelnest.io>
2018-08-02 17:20:22 +0200

    Add share service

---
+++ services/share/index.js
@@ -0,0 +1,28 @@
+const parseRequest = require('../lib/params')
+const getShareId = require('./src/share')
+
+const SHARE_URL = 'https://developer.cloud.unity3d.com/share/'
+
+// -------------------------------------------------------------
+// Module.
+// -------------------------------------------------------------
+
+module.exports = async request => {
+  const {key, link} = parseRequest(request)
+
+  if (!key) {
+    return {code: 401, message: 'Unauthorized: no API Key provided.'}
+  }
+
+  if (!link) {
+    return {code: 401, message: 'Unauthorized: no share link provided.'}
+  }
+
+  try {
+    const shareId = await getShareId(key, link)
+    const shareLink = SHARE_URL + shareId
+    return {link: shareLink}
+  } catch (e) {
+    return {message: e.message}
+  }
+}
---
+++ services/share/src/share.js
@@ -0,0 +1,11 @@
+const {post} = require('../../lib/request')
+
+// -------------------------------------------------------------
+// Module.
+// -------------------------------------------------------------
+
+module.exports = async (key, shareEndpoint) => {
+  return post(shareEndpoint, key)
+    .then(res => res.data)
+    .then(res => res.shareid)
+}
