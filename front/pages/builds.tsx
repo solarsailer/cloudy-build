cd3570c9a05d5397fbc1da408ea3f2dc90b18d04
Matthieu Oger <moger@pixelnest.io>
2018-08-03 15:48:33 +0200

    Add list of builds

--- front/pages/builds.tsx
+++ front/pages/builds.tsx
@@ -1,13 +1,92 @@
-const Page: any = () => {
-  return (
-    <div>
-      <p>List of builds</p>
-    </div>
-  )
-}
+import React from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
 
-Page.getInitialProps = async () => {
-  return {}
-}
+import Build from '../components/Build'
 
-export default Page
+import {tint} from 'polished'
+
+import {colors} from '../styles/config'
+
+// -------------------------------------------------------------
+// Components.
+// ------------------------------------------------------------
+
+const Row = styled.li`
+  margin-bottom: 2rem;
+  padding-bottom: 2rem;
+  border-bottom: 1px solid ${tint(0.25, colors.page.content)};
+
+  &:last-of-type {
+    margin: 0;
+    padding: 0;
+    border: none;
+  }
+`
+
+const ErrorBlock = styled.div`
+  text-align: center;
+
+  color: ${tint(0.5, colors.page.content)};
+
+  .main {
+    margin-bottom: 2rem;
+  }
+
+  span {
+    font-size: 0.75em;
+    font-style: italic;
+  }
+`
+
+// -------------------------------------------------------------
+// Page.
+// -------------------------------------------------------------
+
+export default class extends React.Component<any, any> {
+  static async getInitialProps({query}) {
+    const result = await axios
+      .get('http://localhost:3001/', {
+        params: {
+          key: query.key,
+          org: query.org,
+          project: query.project
+        }
+      })
+      .then(res => res.data)
+
+    if (result.code && result.code !== 200) {
+      return {builds: [], hasError: true, error: result.message}
+    }
+
+    return {builds: result, hasError: false}
+  }
+
+  render() {
+    if (this.props.hasError) {
+      return (
+        <ErrorBlock>
+          <p className="main">
+            Snap! We cannot find any build.<br />
+            <span>({this.props.error})</span>
+          </p>
+          <p>
+            <a href="/">Try Again</a>
+          </p>
+        </ErrorBlock>
+      )
+    }
+
+    return (
+      <ul>
+        {this.props.builds.map(x => (
+          <Row
+            key={`${x.org}-${x.projectId}-${x.buildTypeId}-${x.buildNumber}`}
+          >
+            <Build {...x} />
+          </Row>
+        ))}
+      </ul>
+    )
+  }
+}
