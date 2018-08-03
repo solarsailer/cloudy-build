d6a3d033ee6777af3a5e264dcc933f251d5436b1
Matthieu Oger <moger@pixelnest.io>
2018-08-03 18:11:35 +0200

    Rework spinner

--- front/components/Spinner.tsx
+++ front/components/Spinner.tsx
@@ -1,4 +1,4 @@
-import styled, {keyframes} from 'styled-components'
+import styled from 'styled-components'
 
 import {colors} from '../styles/config'
 
@@ -8,71 +8,72 @@ import {colors} from '../styles/config'
 // Components.
 // -------------------------------------------------------------
 
-const anim1 = keyframes`
-  0% {
-    transform: scale(0);
-  }
-  100% {
-    transform: scale(1);
-  }
-`
-
-const anim2 = keyframes`
-  0% {
-    transform: translate(0, 0);
-  }
-  100% {
-    transform: translate(19px, 0);
-  }
-`
-
-const anim3 = keyframes`
-  0% {
-    transform: scale(1);
-  }
-  100% {
-    transform: scale(0);
-  }
-`
-
 const Container = styled.div`
   display: inline-block;
   position: relative;
 
   width: 64px;
   height: 64px;
-`
 
-const AbstractChild = styled.div`
-  position: absolute;
-  top: 27px;
+  div {
+    position: absolute;
+    top: 27px;
 
-  width: 11px;
-  height: 11px;
+    width: 11px;
+    height: 11px;
 
-  background: ${colors.brand};
-  border-radius: 50%;
+    background: ${colors.brand};
+    border-radius: 50%;
 
-  animation-timing-function: cubic-bezier(0, 1, 1, 0);
-`
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
 
-const Child1 = AbstractChild.extend`
-  left: 6px;
-  animation: ${anim1} 0.6s infinite;
-`
-const Child2 = AbstractChild.extend`
-  left: 6px;
-  animation: ${anim2} 0.6s infinite;
-`
+  div:nth-child(1) {
+    left: 6px;
+    animation: spinner-lds-ellipsis1 0.6s infinite;
+  }
 
-const Child3 = AbstractChild.extend`
-  left: 26px;
-  animation: ${anim2} 0.6s infinite;
-`
+  div:nth-child(2) {
+    left: 6px;
+    animation: spinner-lds-ellipsis2 0.6s infinite;
+  }
 
-const Child4 = AbstractChild.extend`
-  left: 45px;
-  animation: ${anim3} 0.6s infinite;
+  div:nth-child(3) {
+    left: 26px;
+    animation: spinner-lds-ellipsis2 0.6s infinite;
+  }
+
+  div:nth-child(4) {
+    left: 45px;
+    animation: spinner-lds-ellipsis3 0.6s infinite;
+  }
+
+  @keyframes spinner-lds-ellipsis1 {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes spinner-lds-ellipsis3 {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+
+  @keyframes spinner-lds-ellipsis2 {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(19px, 0);
+    }
+  }
 `
 
 // -------------------------------------------------------------
@@ -82,10 +83,10 @@ const Child4 = AbstractChild.extend`
 export default () => {
   return (
     <Container>
-      <Child1 />
-      <Child2 />
-      <Child3 />
-      <Child4 />
+      <div />
+      <div />
+      <div />
+      <div />
     </Container>
   )
 }
